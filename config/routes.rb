3fdef963548bc4b74c9584d81d1d24ecfa4e487d
Ilia <teterailiya@gmail.com>
2024-10-15 10:45:34 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :pages
-    root 'pages#index'
+    resources :pages do
+      resources :comments
+    end
   end
   resources :pages
+  root 'pages#index'
 end
