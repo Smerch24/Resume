1ea4498226d25e96bc65e4052d3233c59fba3abd
Ilia <teterailiya@gmail.com>
2024-10-15 10:45:47 +0300

    Setup comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  class CommentsController < ApplicationController
+    def create
+      @page = Page.find(params[:id])
+      @comment = @page.comments.new(comment_params)
+
+      if @comment.save
+        redirect_to @page
+      else
+        render 'pages/show'
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:name, :content)
+    end
+  end
 end
